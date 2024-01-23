92f23b1eb13088c633071d0b91da3dd532b3efae
novailable <97833342+novailable@users.noreply.github.com>
2024-01-24 05:36:28 +0800

    24 Jan, Wed, 1

--- map.py
+++ map.py
@@ -1,6 +1,5 @@
 import googlemaps
 
-gmaps = googlemaps.Client(key="AIzaSyBVaeNch_aeLuTOIVX75orHHF0z7eeGlJI")
 
 
 def get_user_location():
@@ -16,43 +15,41 @@ def get_user_location():
         print(f"Error: {e}")
         return None
 
+class geo_map():
 
-def get_nearby_spots(location, radius, keyword="birdwatching area"):
-    # Initialize the Google Maps client
+    def __init__(self):
+        self.__API = "AIzaSyBVaeNch_aeLuTOIVX75orHHF0z7eeGlJI"
+        self.__gmaps = googlemaps.Client(key=self.__API)
+        self.__map_id = '16d8ab4a81eec419'
 
-    try:
-        # Search for nearby places with the specified keyword (e.g., "bird watching")
-        places_result = gmaps.places_nearby(
-            location=location,
-            radius=radius,
-            keyword=keyword
-        )
-        # Extract relevant information
-        print(places_result)
-        nearby_spots = []
-        for place in places_result["results"]:
-            name = place.get("name", "N/A")
-            address = place.get("vicinity", "N/A")
-            rating = place.get("rating", "N/A")
-            location = place.get("geometry").get("location")
-            nearby_spots.append({
-                "name": name,
-                "address": address,
-                "rating": rating,
-                "location": location
-            })
+    def get_API_ID(self):
+        return self.__API, self.__map_id
 
-        return places_result
+    def get_nearby_spots(self, location, radius, keyword="birdwatching area"):
+        # Initialize the Google Maps client
+        self.location = location
+        self.radius = radius
+        self.keyword = keyword
 
-    except googlemaps.exceptions.ApiError as e:
-        print(f"Error: {e}")
-        return None
+        try:
+            # Search for nearby places with the specified keyword (e.g., "bird watching")
+            places_result = self.__gmaps.places_nearby(
+                location=self.location,
+                radius=self.radius,
+                keyword=self.keyword
+            )
 
+            nearby_spots = []
+            for place in places_result["results"]:
+                nearby_spots.append({
+                    "name": place.get("name", "N/A"),
+                    "address": place.get("vicinity", "N/A"),
+                    "rating": place.get("rating", "N/A"),
+                    "location": place.get("geometry").get("location")
+                })
 
-if __name__ == '__main__':
-    loc = get_user_location()
-    lat = loc.get("lat")
-    lng = loc.get("lng")
-    map = get_nearby_spots(loc)
-    for m in map:
-        print(m)
+            return places_result
+
+        except googlemaps.exceptions.ApiError as e:
+            print(f"Error: {e}")
+            return None
